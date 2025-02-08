671bf4293cd4b1e1e0d2ba9096295bca64b600ae
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-02-08 00:08:53 -0300

    feat: add pre-commit hook and configure lint-staged for automatic linting

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,7 +2,7 @@
 
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Environment, SoftShadows } from "@react-three/drei";
-import { Suspense, useRef, useState, useEffect } from "react";
+import { Suspense, useRef } from "react";
 import * as THREE from "three";
 
 interface CubeProps {
