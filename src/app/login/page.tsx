73252d95599e146e2192514bc17c00f7c981da62
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-16 00:50:49 -0300

    feat: update author display text in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          By Matias
+          By Me
         </a>
         <LoginForm handleLogin={handleLogin} />
       </div>
