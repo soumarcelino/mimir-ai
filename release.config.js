50977f63fe2195c8635fba47f70cfef14fa9c735
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-16 18:35:27 -0300

    feat: add GitHub Actions workflow for Slack notifications on release

--- release.config.js
+++ release.config.js
@@ -4,14 +4,6 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/npm",
     "@semantic-release/release-notes-generator",
-    [
-      "semantic-release-slack-bot",
-      {
-        notifyOnSuccess: true,
-        notifyOnFail: true,
-        slackWebhook: process.env.SLACK_WEBHOOK,
-      },
-    ],
     "@semantic-release/changelog",
     [
       "@semantic-release/exec",
