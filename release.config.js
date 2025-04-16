0b030886879d67d22101165dbb002aa58632fbfe
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-16 14:29:21 -0300

    fix: enable failure notifications for Slack bot

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
       "semantic-release-slack-bot",
       {
         notifyOnSuccess: true,
-        notifyOnFail: false,
+        notifyOnFail: true,
         slackWebhook: process.env.SLACK_WEBHOOK,
       },
     ],
