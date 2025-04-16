55e6ea545852ba07f0f0691845403c176eb682df
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-16 00:20:04 -0300

    feat: reorganize release.config.js for improved plugin structure

--- release.config.js
+++ release.config.js
@@ -1,25 +1,23 @@
 module.exports = {
   branches: ["main"],
   plugins: [
+    "@semantic-release/commit-analyzer",
     [
-      "@semantic-release/commit-analyzer",
-      [
-        "@semantic-release/exec",
-        {
-          verifyReleaseCmd:
-            'echo "NEXT_RELEASE_VERSION=${nextRelease.version}" >> $GITHUB_ENV',
-        },
-      ],
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github",
-      [
-        "@semantic-release/git",
-        {
-          assets: ["package.json", "yarn.lock", "CHANGELOG.md"],
-          message: "chore(release): ${nextRelease.version} [skip ci]",
-        },
-      ],
-      "@semantic-release/changelog",
+      "@semantic-release/exec",
+      {
+        verifyReleaseCmd:
+          'echo "NEXT_RELEASE_VERSION=${nextRelease.version}" >> $GITHUB_ENV',
+      },
     ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "yarn.lock", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+    "@semantic-release/changelog",
   ],
 };
