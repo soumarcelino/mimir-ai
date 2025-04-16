19b5b68bd496a56e7735b7ca8209a53939ce597e
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-15 23:30:19 -0300

    fix: update release:update-package-only script to use extends and enhance release configuration

--- release.config.js
+++ release.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   branches: ["main"],
-  plugins: ["@semantic-release/commit-analyzer"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/exec",
+    "@semantic-release/git",
+    "@semantic-release/changelog",
+  ],
 };
