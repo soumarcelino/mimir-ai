f9acadb25d1e445080d94ec1f62969ad3412d318
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2025-04-16 12:44:04 -0300

    feat: inventei a roda

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
-          By Me
+          By Matias
         </a>
         <LoginForm handleLogin={handleLogin} />
       </div>
